78c1c2ade3484edc6239c07ffba399ed63f6f374
Pavel Yosifovich <zodiacon@live.com>
2019-07-09 19:53:53 +0300

    Add project files.

---
+++ Chapter01/HelloWin/HelloWin.cpp
@@ -0,0 +1,17 @@
+// HelloWin.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+
+int main() {
+	SYSTEM_INFO si;
+	::GetSystemInfo(&si);
+
+	printf("Number of Logical Processors: %d\n", si.dwNumberOfProcessors);
+	printf("Page size: %d Bytes\n", si.dwPageSize);
+	printf("Processor Mask: %016X\n", si.dwActiveProcessorMask);
+	printf("Minimum process address: 0x%p\n", si.lpMinimumApplicationAddress);
+	printf("Maximum process address: 0x%p\n", si.lpMaximumApplicationAddress);
+
+	return 0;
+}
