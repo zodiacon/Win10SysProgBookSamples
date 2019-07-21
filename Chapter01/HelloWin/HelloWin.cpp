9d18f377109355bf6c4cac50144da7da7b1b8398
Pavel Yosifovich <zodiacon@live.com>
2019-07-21 07:48:53 +0300

    fixed 32/64 bit bug

--- Chapter01/HelloWin/HelloWin.cpp
+++ Chapter01/HelloWin/HelloWin.cpp
@@ -9,7 +9,11 @@ int main() {
 
 	printf("Number of Logical Processors: %d\n", si.dwNumberOfProcessors);
 	printf("Page size: %d Bytes\n", si.dwPageSize);
-	printf("Processor Mask: %016llX\n", (ULONGLONG)si.dwActiveProcessorMask);
+#ifdef _WIN64
+		printf("Processor Mask: %016llX\n", si.dwActiveProcessorMask);
+#else
+		printf("Processor Mask: %08X\n", si.dwActiveProcessorMask);
+#endif
 	printf("Minimum process address: 0x%p\n", si.lpMinimumApplicationAddress);
 	printf("Maximum process address: 0x%p\n", si.lpMaximumApplicationAddress);
 
