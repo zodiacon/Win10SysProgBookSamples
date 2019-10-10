d8f892d0bec940bdbebd3e730228655d826b0348
Pavel Yosifovich <zodiacon@live.com>
2019-10-10 20:48:05 +0200

    JobTree sample application

---
+++ Chapter04/JobTree/JobTree.cpp
@@ -0,0 +1,56 @@
+// JobTree.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <windows.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string>
+
+HANDLE CreateSimpleProcess(PCWSTR name) {
+	std::wstring sname(name);
+	PROCESS_INFORMATION pi;
+	STARTUPINFO si = { sizeof(si) };
+	if (!::CreateProcess(nullptr, const_cast<PWSTR>(sname.data()), nullptr, nullptr, FALSE, 
+		CREATE_BREAKAWAY_FROM_JOB | CREATE_NEW_CONSOLE,
+		nullptr, nullptr, &si, &pi))
+		return nullptr;
+
+	::CloseHandle(pi.hThread);
+	return pi.hProcess;
+}
+
+HANDLE CreateJobHierarchy() {
+	auto hJob1 = ::CreateJobObject(nullptr, L"Job1");
+	assert(hJob1);
+	auto hProcess1 = CreateSimpleProcess(L"mspaint");
+
+	auto success = ::AssignProcessToJobObject(hJob1, hProcess1);
+	assert(success);
+
+	auto hJob2 = ::CreateJobObject(nullptr, L"Job2");
+	assert(hJob2);
+	success = ::AssignProcessToJobObject(hJob2, hProcess1);
+	assert(success);
+
+	auto hProcess2 = CreateSimpleProcess(L"mstsc");
+	success = ::AssignProcessToJobObject(hJob2, hProcess2);
+	assert(success);
+
+	auto hProcess3 = CreateSimpleProcess(L"cmd");
+	success = ::AssignProcessToJobObject(hJob1, hProcess3);
+	assert(success);
+
+	::CloseHandle(hProcess1);
+	::CloseHandle(hProcess2);
+	::CloseHandle(hProcess3);
+
+	return hJob1;
+}
+
+int main() {
+	auto hJob = CreateJobHierarchy();
+	printf("Press any key to terminate parent job...\n");
+	::getchar();
+	::TerminateJobObject(hJob, 0);
+}
+
