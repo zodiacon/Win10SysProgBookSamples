ecca37a6c27b2007b3cb7b8e41009308b81f47eb
Pavel Yosifovich <zodiacon@live.com>
2020-03-20 20:55:12 +0200

    Added the Simple Work application

---
+++ Chapter09/SimpleWork/MainDlg.cpp
@@ -0,0 +1,123 @@
+// MainDlg.cpp : implementation of the CMainDlg class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "resource.h"
+
+#include "MainDlg.h"
+
+extern "C" NTSTATUS NTAPI NtGetNextThread(
+	_In_ HANDLE ProcessHandle,
+	_In_opt_ HANDLE ThreadHandle,
+	_In_ ACCESS_MASK DesiredAccess,
+	_In_ ULONG HandleAttributes,
+	_In_ ULONG Flags,
+	_Out_ PHANDLE NewThreadHandle);
+
+void CMainDlg::OnCallback(PTP_CALLBACK_INSTANCE instance, PVOID context) {
+	auto dlg = (CMainDlg*)context;
+
+	// post message indicating start
+	dlg->PostMessage(WM_APP + 1, ::GetCurrentThreadId());
+
+	// simulate work...
+	::Sleep(10 * (::GetTickCount() & 0xff));
+
+	// post message indicating end
+	dlg->PostMessage(WM_APP + 2, ::GetCurrentThreadId());
+}
+
+LRESULT CMainDlg::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/) {
+	// center the dialog on the screen
+	CenterWindow();
+
+	m_List.Attach(GetDlgItem(IDC_WORKITEMS));
+
+	// set icons
+	HICON hIcon = AtlLoadIconImage(IDR_MAINFRAME, 0, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON));
+	SetIcon(hIcon, TRUE);
+	HICON hIconSmall = AtlLoadIconImage(IDR_MAINFRAME, 0, ::GetSystemMetrics(SM_CXSMICON), ::GetSystemMetrics(SM_CYSMICON));
+	SetIcon(hIconSmall, FALSE);
+
+	SetTimer(1, 2000, nullptr);
+
+	return TRUE;
+}
+
+LRESULT CMainDlg::OnTimer(UINT, WPARAM id, LPARAM, BOOL&) {
+	if (id != 1)
+		return 0;
+
+	// enumerate processes
+	auto hSnapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnapshot == INVALID_HANDLE_VALUE)
+		return 0;
+
+	PROCESSENTRY32 pe;
+	pe.dwSize = sizeof(pe);
+
+	::Process32First(hSnapshot, &pe);
+
+	auto pid = ::GetCurrentProcessId();
+	ULONG threads = 0;
+	while (::Process32Next(hSnapshot, &pe)) {
+		if (pe.th32ProcessID == pid) {
+			threads = pe.cntThreads;
+			break;
+		}
+	}
+	::CloseHandle(hSnapshot);
+
+	CString text;
+	text.Format(L"Threads: %u\n", threads);
+	SetDlgItemText(IDC_THREADS, text);
+
+	return 0;
+}
+
+LRESULT CMainDlg::OnCallbackStart(UINT, WPARAM wParam, LPARAM, BOOL&) {
+	CString text;
+	text.Format(L"Started on thread %d", wParam);
+	m_List.AddString(text);
+
+	return 0;
+}
+
+LRESULT CMainDlg::OnCallbackEnd(UINT, WPARAM wParam, LPARAM, BOOL&) {
+	CString text;
+	text.Format(L">>ended on thread %d", wParam);
+	m_List.AddString(text);
+
+	return 0;
+}
+
+LRESULT CMainDlg::OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	EndDialog(wID);
+	return 0;
+}
+
+LRESULT CMainDlg::OnSubmitWorkItem(WORD, WORD wID, HWND, BOOL&) {
+	if(!::TrySubmitThreadpoolCallback(OnCallback, this, nullptr))
+		AtlMessageBox(*this, L"Failed to submit work item callback", 
+			IDR_MAINFRAME, MB_ICONERROR);
+
+	return 0;
+}
+
+LRESULT CMainDlg::OnSubmit10WorkItems(WORD, WORD, HWND, BOOL&) {
+	for (int i = 0; i < 10; i++) {
+		if (!::TrySubmitThreadpoolCallback(OnCallback, this, nullptr)) {
+			AtlMessageBox(*this, L"Failed to submit work item callback", 
+				IDR_MAINFRAME, MB_ICONERROR);
+			break;
+		}
+	}
+	return 0;
+}
+
+LRESULT CMainDlg::OnClear(WORD, WORD wID, HWND, BOOL&) {
+	m_List.ResetContent();
+
+	return 0;
+}
---
+++ Chapter09/SimpleWork/SimpleWork.h
@@ -0,0 +1,1 @@
+// SimpleWork.h
