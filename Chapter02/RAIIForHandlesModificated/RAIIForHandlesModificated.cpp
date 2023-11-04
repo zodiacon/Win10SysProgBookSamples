3a0815b47280e8e6719df7fa9f3083b52766607a
shala'la <58109772+shalala66@users.noreply.github.com>
2023-11-04 23:55:59 +0400

    RAII for Handles Modificated Version
    Modification kindred with addings (all tested in Visual Studio 2019):
    1) "noexcept" operator to the necessary operations, such as destructor, move constructor and move assignment operator. Due to there was generates warning C26439:
    https://learn.microsoft.com/en-us/cpp/code-quality/c26439?view=msvc-170#:~:text=Special%20kinds%20of%20operations
    
    Importance of adding noexcept operations: 
    https://en.cppreference.com/w/cpp/language/operators#:~:text=%26%26%20other)-,noexcept,-%7B%0A%20%20%20%20//%20Guard%20self
    https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rf-noexcept:~:text=low%2Dlevel%20functions.-,Note,-Destructors%2C%20swap
    https://en.cppreference.com/w/cpp/language/noexcept#:~:text=The%20noexcept%20operator%20performs%20a,some%20types%20but%20not%20others.
    
    2) The main function have to return type an "int" in general, because of that is why there was a compile error C2561 which solved via changing the "return;" line to "return 1;" for int main() function: 
    https://learn.microsoft.com/en-us/cpp/error-messages/compiler-errors-2/compiler-error-c2561?view=msvc-170&f1url=%3FappId%3DDev16IDEF1%26l%3DRU-RU%26k%3Dk(C2561)%26rd%3Dtrue
    https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-1-c4326?view=msvc-170&f1url=%3FappId%3DDev16IDEF1%26l%3DRU-RU%26k%3Dk(C4326)%26rd%3Dtrue
    
    3) The "Get()" method was replaced with "operator()" overloading related with adding implicit conversion to HANDLE as you mentioned:
    HANDLE operator()() const {
            return _h;
        }

---
+++ Chapter02/RAIIForHandlesModificated/RAIIForHandlesModificated.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct Handle {
+    explicit Handle(HANDLE h = nullptr) : _h(h) {}
+
+    ~Handle() noexcept { Close(); }
+
+    // delete copy-ctor and copy-assignment
+    Handle(const Handle&) = delete;
+    Handle& operator=(const Handle&) = delete;
+
+    // allow move (transfer ownership)
+    Handle(Handle&& other) noexcept : _h(other._h) {
+        other._h = nullptr;
+        // *this = std::move(other);
+    }
+    Handle& operator=(Handle&& other) noexcept {
+        if (this != &other) {
+            Close();
+            _h = other._h;
+            other._h = nullptr;
+        }
+        return *this;
+    }
+
+    operator bool() const {
+        return _h != nullptr && _h != INVALID_HANDLE_VALUE;
+    }
+
+    HANDLE operator()() const {
+        return _h;
+    }
+
+    void Close() {
+        if (_h) {
+            ::CloseHandle(_h);
+            _h = nullptr;
+        }
+    }
+
+private:
+    HANDLE _h;
+};
+
+
+int main()
+{
+    Handle hMyEvent(::CreateEvent(nullptr, TRUE, FALSE, nullptr));
+    if (!hMyEvent) {
+        // handle failure
+        return 1;
+    }
+    ::SetEvent(hMyEvent());
+
+    // move ownership
+    Handle hOtherEvent(std::move(hMyEvent));
+    ::ResetEvent(hOtherEvent());
+
+    return 0;
+}
